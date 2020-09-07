d780ac2358b9babc3fbdd0a08bfc1ad544a676b5
nova <nova@roadbeats.com>
2020-09-07 13:25:33 +0200

    setup the model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
-class Contact < ApplicationRecord
+class Contact < MailForm::Base
+  attribute :name, validate: true
+  attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
+  attribute :message, validate: true
+  # attribute :nickname, captcha: true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      subject: "Contact Form",
+      to: "your_email@gmail.com",
+      from: %("#{name}" <#{email}>)
+    }
+  end
 end

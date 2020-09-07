c968edcdb83a9b0aba506c9a9bdf0c2ea7f21e0c
nova <nova@roadbeats.com>
2020-09-07 13:28:03 +0200

    setup the controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,24 @@
 class ContactsController < ApplicationController
+  require 'mail_form'
+  invisible_captcha only: [:create, :update], honeypot: :subtitle
+
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = 'Cannot send message'
+      render 'contacts/new'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message, :subtitle)
   end
 end
